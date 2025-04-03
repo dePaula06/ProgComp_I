e2b328373f21383f3f75229b529cf8eb3ab7c1ef
dePaula06 <p.depaula06.sp@gmail.com>
2025-04-02 22:06:22 -0300

    new: cálculo kits e suplementos por equipe

--- troteSolidario/js/index.js
+++ troteSolidario/js/index.js
@@ -1,16 +1,118 @@
 function calcular(){
-    // recupera o valor da ação social digitado
+    // declaração variável sem tipo
+   let soma = 0
+   // recupera o valor da ação social digitado
+   let acaoSocial = document.getElementById("acaoSocial").value
+   soma = soma + Number(acaoSocial)
+   // recupera o valor da homenagem digitado
+   let homenagem = document.getElementById("homenagem").value
+   soma = soma + Number(homenagem)
+   // recupera a qtde de litros de leite
+   let leite = document.getElementById("leite").value
+   soma = soma + (2 * Number(leite))
+   // recupera a qtde de kits 
+   let kit = Number(document.getElementById("kit").value)
+   // recupera a cor da equipe
+   let equipe = document.getElementById("equipe").value
+    // recupera a qtde de latas de suplemento
+   let suplemento = document.getElementById("suplemento").value
+   let pontosKitSupl = 0 // guarda pontuação de kit + supl
+   if (equipe == "Laranja"){
+       // equipe é laranja
+       if (kit >= 97 && suplemento >= 49) {
+           pontosKitSupl = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
+       }
+       else if (kit >= 78 && suplemento >=39){
+           pontosKitSupl = 4000 + ((kit - 78) * 30)  + ((suplemento - 39) * 15)
+       }
+       else if (kit >= 49 && suplemento >=25) {
+           pontosKitSupl = 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+       }
+       else if (kit >= 19 && suplemento >=10){
+           pontosKitSupl = 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+       }
+       else {
+           pontosKitSupl = 0
+       }
+   }
 
-    //declaração de variável sem tipo (JS não é tipável)
-    let acaoSocial = document.getElementById("acaoSocial").value
+   if (equipe == "Preta"){
+    // equipe é preta
+    if (kit >= 103 && suplemento >= 52) {
+        pontosKitSupl = 5000 + ((kit - 103) * 30) + ((suplemento - 52) * 15)
+    }
+    else if (kit >= 82 && suplemento >=42){
+        pontosKitSupl = 4000 + ((kit - 82) * 30)  + ((suplemento - 42) * 15)
+    }
+    else if (kit >= 52 && suplemento >=26) {
+        pontosKitSupl = 2500 + ((kit - 52) * 30) + ((suplemento - 26) * 15)
+    }
+    else if (kit >= 21 && suplemento >=10){
+        pontosKitSupl = 1000 + ((kit - 21) * 30) + ((suplemento - 10) * 15)
+    }
+    else {
+        pontosKitSupl = 0
+    }
+}
 
-    //recupera o valor digitado em homenagem
-    let homenagem = document.getElementById("homenagem").value
+if (equipe == "Roxa"){
+    // equipe é roxa
+    if (kit >= 102 && suplemento >= 51) {
+        pontosKitSupl = 5000 + ((kit - 102) * 30) + ((suplemento - 51) * 15)
+    }
+    else if (kit >= 82 && suplemento >=41){
+        pontosKitSupl = 4000 + ((kit - 82) * 30)  + ((suplemento - 41) * 15)
+    }
+    else if (kit >= 51 && suplemento >=26) {
+        pontosKitSupl = 2500 + ((kit - 51) * 30) + ((suplemento - 26) * 15)
+    }
+    else if (kit >= 20 && suplemento >=10){
+        pontosKitSupl = 1000 + ((kit - 20) * 30) + ((suplemento - 10) * 15)
+    }
+    else {
+        pontosKitSupl = 0
+    }
+}
 
-    acaoSocial = Number(acaoSocial)
-    homenagem = Number(homenagem)
+if (equipe == "Verde"){
+    // equipe é verde
+    if (kit >= 88 && suplemento >= 44) {
+        pontosKitSupl = 5000 + ((kit - 88) * 30) + ((suplemento - 44) * 15)
+    }
+    else if (kit >= 70 && suplemento >=35){
+        pontosKitSupl = 4000 + ((kit - 70) * 30)  + ((suplemento - 35) * 15)
+    }
+    else if (kit >= 44 && suplemento >=22) {
+        pontosKitSupl = 2500 + ((kit - 44) * 30) + ((suplemento - 22) * 15)
+    }
+    else if (kit >= 18 && suplemento >=9){
+        pontosKitSupl = 1000 + ((kit - 18) * 30) + ((suplemento - 9) * 15)
+    }
+    else {
+        pontosKitSupl = 0
+    }
+}
 
-    let soma = acaoSocial + homenagem
-
-    alert(soma)
+if (equipe == "Vermelha"){
+    // equipe é vermelha
+    if (kit >= 93 && suplemento >= 47) {
+        pontosKitSupl = 5000 + ((kit - 93) * 30) + ((suplemento - 47) * 15)
+    }
+    else if (kit >= 74 && suplemento >=38){
+        pontosKitSupl = 4000 + ((kit - 74) * 30)  + ((suplemento - 38) * 15)
+    }
+    else if (kit >= 47 && suplemento >=24) {
+        pontosKitSupl = 2500 + ((kit - 47) * 30) + ((suplemento - 24) * 15)
+    }
+    else if (kit >= 19 && suplemento >=9){
+        pontosKitSupl = 1000 + ((kit - 19) * 30) + ((suplemento - 9) * 15)
+    }
+    else {
+        pontosKitSupl = 0
+    }
+}
+   soma = soma + pontosKitSupl
+  
+   // devolve o resultado para o HTML
+   document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
